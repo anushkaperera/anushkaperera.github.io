834effea6c7b7f2b129745733af8f9323ab2a662
Anushka Perera <anushka.uom.tuc@gmail.com>
2025-02-21 03:05:08 +0100

    Update simpledae.py

--- code/python/pyomo/basic/simpledae.py
+++ code/python/pyomo/basic/simpledae.py
@@ -1,11 +1,13 @@
+import  matplotlib.pyplot as plt 
+
 from pyomo.environ import *
 from pyomo.dae import *
 
 
-mu = 1.0
+mu = 5.0
 
 m = ConcreteModel()
-m.t = ContinuousSet(bounds=(0.0, 1.0))
+m.t = ContinuousSet(bounds=(0.0, 10.0))
 m.x = Var(m.t)
 m.y = Var(m.t)
 m.x_dot = DerivativeVar(m.x)
@@ -20,8 +22,15 @@ def ode_y(m, t):
     return m.y_dot[t] == mu * (1.0 - m.x[t] * m.x[t]) * m.y[t] - m.x[t]
 
 m.pc = ConstraintList()
-m.pc.add(m.x[0]==1.0)
-m.pc.add(m.y[0]==-1.0)
+m.pc.add(m.x[0]==10.0)
+m.pc.add(m.y[0]==-10.0)
 
-TransformationFactory('dae.finite_difference').apply_to(m, wrt=m.t, nfe=30)
+TransformationFactory('dae.collocation').apply_to(m, nfe =50, ncp =6, scheme='LAGRANGE-RADAU')
 SolverFactory('ipopt').solve(m).write()
+
+plt.figure()
+plt.plot(m.x.extract_values().values(), m.y.extract_values().values(), '.')
+plt.figure()
+plt.plot(m.t.data(), m.x.extract_values().values(), '.')
+plt.plot(m.t.data(), m.y.extract_values().values(), '.')
+plt.show()
