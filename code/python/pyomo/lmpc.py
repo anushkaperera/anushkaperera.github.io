1a6bcb334212cb781e51968a3f7f7c26b5588735
Anushka Perera <anushka.uom.tuc@gmail.com>
2025-02-23 21:03:23 +0100

    Update lmpc.py

--- code/python/pyomo/lmpc.py
+++ code/python/pyomo/lmpc.py
@@ -1,1 +1,121 @@
+from itertools import chain
+import logging
+from mpl_toolkits.mplot3d import Axes3D  
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import random
+from scipy.interpolate import interp2d
+from scipy.integrate import odeint
 
+import shutil
+import sys
+import os.path
+
+if not shutil.which("pyomo"):
+    !pip install -q pyomo
+    assert(shutil.which("pyomo"))
+
+if not (shutil.which("cbc") or os.path.isfile("cbc")):
+    if "google.colab" in sys.modules:
+        !apt-get install -y -qq coinor-cbc
+    else:
+        try:
+            !conda install -c conda-forge coincbc 
+        except:
+            pass
+
+assert(shutil.which("cbc") or os.path.isfile("cbc"))
+
+from pyomo.environ import *
+
+
+def mpc_double_integrator(N=2, h=1):
+    m = ConcreteModel()
+    m.states = RangeSet(1, 2)
+    m.k = RangeSet(0, N)
+    
+    m.h = Param(initialize=h, mutable=True)
+    m.ic = Param(m.states, initialize={1:0.5, 2:0.5}, mutable=True)
+    m.gamma = Param(default=0.5, mutable=True)
+    
+    m.x = Var(m.states, m.k)
+    m.icfix = Constraint(m.states, rule = lambda m, i: m.x[i,0] == m.ic[i])
+    m.x[1,N].fix(0)
+    m.x[2,N].fix(0)
+    
+    m.u = Var(m.k, bounds=(-1, 1))
+    m.upos = Var(m.k, bounds=(0, 1))
+    m.uneg = Var(m.k, bounds=(0, 1))
+    m.usum = Constraint(m.k, rule = lambda m, k: m.u[k] == m.upos[k] - m.uneg[k])   
+  
+    m.y = Var(m.k, bounds=(-1, 1))
+    m.ypos = Var(m.k, bounds=(0, 1))
+    m.yneg = Var(m.k, bounds=(0, 1))
+    m.ysum = Constraint(m.k, rule = lambda m, k: m.y[k] == m.ypos[k] - m.yneg[k])
+
+    m.x1_update = Constraint(m.k, rule = lambda m, k:
+           m.x[1,k+1] == m.x[1,k] + m.h*m.x[2,k] + m.h**2*m.u[k]/2 if k < N else Constraint.Skip)
+    m.x2_update = Constraint(m.k, rule = lambda m, k:
+           m.x[2,k+1] == m.x[2,k] + m.h*m.u[k] if k < N else Constraint.Skip)
+    m.y_output = Constraint(m.k, rule = lambda m, k: m.y[k] == m.x[1,k])
+    
+    m.uobj = m.gamma*sum(m.upos[k] + m.uneg[k] for k in m.k)
+    m.yobj = (1-m.gamma)*sum(m.ypos[k] + m.yneg[k] for k in m.k)
+    m.obj = Objective(expr = m.uobj + m.yobj, sense=minimize)
+    
+    return m
+
+def plot_results(m):
+    results = SolverFactory('cbc').solve(m)
+    if str(results.solver.termination_condition) != "optimal":
+        print(results.solver.termination_condition)
+        return
+    
+    # solution data at sample times
+    h = m.h()
+    K = np.array([k for k in m.k])  
+    u = [m.u[k]() for k in K]
+    y = [m.y[k]() for k in K]
+    v = [m.x[2,k]() for k in K]
+    
+    # interpolate between sample times
+    t = np.linspace(0, h) 
+    tp = [_ for _ in chain.from_iterable(k*h + t for k in K[:-1])]
+    up = [_ for _ in chain.from_iterable(u[k] + t*0 for k in K[:-1])]
+    yp = [_ for _ in chain.from_iterable(y[k] + t*(v[k] + t*u[k]/2) for k in K[:-1])]
+    vp = [_ for _ in chain.from_iterable(v[k] + t*u[k] for k in K[:-1])]
+
+    fig = plt.figure(figsize=(10,5))
+    
+    ax1 = fig.add_subplot(3, 2, 1)
+    ax1.plot(tp, yp, 'r--', h*K, y, 'bo')
+    ax1.set_title('position')
+
+    ax2 = fig.add_subplot(3, 2, 3)
+    ax2.plot(tp, vp, 'r--', h*K, v, 'bo')
+    ax2.set_title('velocity')
+
+    ax3 = fig.add_subplot(3, 2, 5)
+    ax3.plot(np.append(tp, K[-1]*h), np.append(up, u[-1]), 'r--', h*K, u, 'bo')
+    ax3.set_title('control force  u[0] = {0:<6.3f}'.format(u[0]))
+
+    ax4 = fig.add_subplot(1, 2, 2)
+    ax4.plot(yp, vp, 'r--', y, v, 'bo')
+    ax4.set_xlim([-1.1, 1.1])
+    ax4.set_aspect('equal', 'box')
+    ax4.set_title('phase plane')
+    ax4.set_xlabel('position')
+    ax4.set_ylabel('velocity')
+    
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.set_ylim(-1.1, 1.1)
+        ax.grid(True)
+    fig.tight_layout()
+
+model = mpc_double_integrator(5, 0.5)
+model.ic[1] = 1.0
+model.ic[2] = 0.2
+
+SolverFactory('cbc').solve(model)
+plot_results(model)
